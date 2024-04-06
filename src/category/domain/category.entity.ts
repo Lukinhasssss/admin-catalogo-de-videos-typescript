c5ad4f75884155a3e347d3210ac030598bc02326
Lucas Monteiro <lukasmonteir0@hotmail.com>
2024-04-06 13:47:24 -0300

    feat: create validation class for category

--- src/category/domain/category.entity.ts
+++ src/category/domain/category.entity.ts
@@ -1,4 +1,5 @@
 import { Uuid } from '../../shared/domain/value-objects/uuid.vo'
+import { CategoryValidatorFactory } from './category.validator'
 
 export type CategoryConstructorProps = {
   categoryId?: Uuid
@@ -48,6 +49,11 @@ export class Category {
 
   deactivate() {
     this.isActive = false
+  }
+
+  static validate(entity: Category) {
+    const validator = CategoryValidatorFactory.create()
+    return validator.validate(entity)
   }
 
   toJSON() {
