793a37c717497cca31d957b61036fd111a8d18fd
Lucas Monteiro <lukasmonteir0@hotmail.com>
2024-04-06 11:04:41 -0300

    test: add tests for other category operations

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 ## Comandos utilizados durante a criação do projeto
 
 ```bash
+yarn init -2
+yarn dlx @yarnpkg/sdks vscode # Este comando irá baixar o pacote @yarnpkg/sdks e executar o comando vscode, que deve configurar o VSCode para usar o SDK do TypeScript do Yarn PnP.
 yarn add typescript -D
 yarn tsc --init
 yarn add jest @types/jest -D
--- src/domain/__tests__/category.entity.spec.ts
+++ src/domain/__tests__/category.entity.spec.ts
@@ -14,7 +14,7 @@ describe('Category Unit Tests', () => {
       expect(category.createdAt).toBeInstanceOf(Date)
     })
 
-    it('should create a category with name and description', () => {
+    it('should create a category with description', () => {
       const category = new Category({
         name: 'Movie',
         description: 'Movie description'
@@ -42,6 +42,119 @@ describe('Category Unit Tests', () => {
       expect(category.description).toBe('Movie description')
       expect(category.isActive).toBeFalsy()
       expect(category.createdAt).toBe(createdAt)
+    })
+  })
+
+  describe('create command', () => {
+    it('should create a category', () => {
+      const category = Category.create({
+        name: 'Movie'
+      })
+
+      expect(category.categoryId).toBeUndefined()
+      expect(category.name).toBe('Movie')
+      expect(category.description).toBeNull()
+      expect(category.isActive).toBeTruthy()
+      expect(category.createdAt).toBeInstanceOf(Date)
+    })
+
+    it('should create a category with description', () => {
+      const category = Category.create({
+        name: 'Movie',
+        description: 'Movie description'
+      })
+
+      expect(category.categoryId).toBeUndefined()
+      expect(category.name).toBe('Movie')
+      expect(category.description).toBe('Movie description')
+      expect(category.isActive).toBeTruthy()
+      expect(category.createdAt).toBeInstanceOf(Date)
+    })
+
+    it('should create a category with isActive', () => {
+      const category = Category.create({
+        name: 'Movie',
+        isActive: false
+      })
+
+      expect(category.categoryId).toBeUndefined()
+      expect(category.name).toBe('Movie')
+      expect(category.description).toBeNull()
+      expect(category.isActive).toBeFalsy()
+      expect(category.createdAt).toBeInstanceOf(Date)
+    })
+
+    it('should create a category with all values', () => {
+      const category = Category.create({
+        name: 'Movie',
+        description: 'Movie description',
+        isActive: false
+      })
+
+      expect(category.categoryId).toBeUndefined()
+      expect(category.name).toBe('Movie')
+      expect(category.description).toBe('Movie description')
+      expect(category.isActive).toBeFalsy()
+      expect(category.createdAt).toBeInstanceOf(Date)
+    })
+  })
+
+  describe('methods', () => {
+    it('should change name', () => {
+      const category = Category.create({
+        name: 'Movie'
+      })
+
+      category.changeName('Music')
+
+      expect(category.name).toBe('Music')
+    })
+
+    it('should change description', () => {
+      const category = Category.create({
+        name: 'Movie'
+      })
+
+      category.changeDescription('Music description')
+
+      expect(category.description).toBe('Music description')
+    })
+
+    it('should activate', () => {
+      const category = Category.create({
+        name: 'Movie',
+        isActive: false
+      })
+
+      category.activate()
+
+      expect(category.isActive).toBeTruthy()
+    })
+
+    it('should deactivate', () => {
+      const category = Category.create({
+        name: 'Movie'
+      })
+
+      category.deactivate()
+
+      expect(category.isActive).toBeFalsy()
+    })
+
+    it('should return JSON', () => {
+      const category = Category.create({
+        name: 'Movie'
+      })
+
+      const json = category.toJSON()
+
+      expect(json).toEqual({
+        categoryId: undefined,
+        name: 'Movie',
+        description: null,
+        isActive: true,
+        createdAt: category.createdAt
+      })
     })
   })
 })
